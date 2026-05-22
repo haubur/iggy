07ef02533a00cc110554b731ae05f49a5654e694
haubur <haubur@mailbox.org>
2026-05-22 18:50:33 +0200

    lib.rs docs: intro and mental model sections

--- core/sdk/src/lib.rs
+++ core/sdk/src/lib.rs
@@ -16,13 +16,34 @@
  * under the License.
  */
 
-//! # Apache Iggy SDK
+// * What, Why, How
+// * Extensive use of links. Maybe use a linkchecker (github action: https://github.com/lycheeverse/lychee-action/)
+
 //! Apache Iggy is a high-performance, persistent message streaming platform written in Rust, capable of processing millions of messages per second with ultra-low latency.
-//! It is part of the [`Incubating Program`] of the [`Apache Software Foundation`] (ASF).
+//! It is part of the [`Incubating Program`] of the [`Apache Software Foundation (ASF)`].
+//!
+//! If you are not new to message streaming, you can safely skip the sections [`Introduction`] and [`Mental Model`].
+//! TODO(haubur): Have a look at the API tiers and persona instead.
+//!
+//! # Introduction
+//! Message-streaming solutions are relevant in contexts where a lot of data is exchanged between many applications running on different devices.
+//! When sending and retrieving data between devices one wants to ensure, that the source device sends the data and the destination device receives the data.
+//! In distributed environments, where many systems, services and devices need to exchange data this can become error prone. Connections can fail, data can be corrupted or devices might fail entirely.
+//!
+//! Message-streaming solutions like Apache Iggy are used when a lot of data needs to be exchanged between services and devices. Usually, these data objects are called messages or events.
+//! A system that depends on many interconnected applications ensuring data consistent data exchange is one of the key concerns. Two major challenges in these environments are *reliability* and *performance*.
+//! Consider the following example: When you drive your vehicle around town many sensors are active and send telemetry data back to the manufacturer. This data is consumed and analyzed by many teams such as aftersales, product teams, production, development, data science and many more.
+//! Imagine hundred thousands of cars each sending the sensor data to each team and their analyis applications individually.
+//! This would likely allow a lot of points of failure. Sending sensors and receiving applications can be temporarily be unavailable or fail entirely, replicating the same data multiple times
+//! to send it via peer-to-peer connections has serious performance implications.[^note]
+//! To circumvent reliability and performance issues, sensors that produce events do not send data directly to the consuming applications. Instead they go through a data buffer. A server where the data is temporarily stored in a very simple data structure.
+//! This is conceptionally not very different from typical data bases like Postgres and others.[^note] But, both serve different use cases. Data bases allow CRUD operations on complex data models and efficient querying. Message-streaming in contrast
+//! is I/O optimized and build to temporarily park raw data that can easily be polled. Hence, on a technical level, the interfaces and gurantees vary substantially and are optimized for the intended use cases.
+//! Visit our website for a [deep dive into the append-only data structure]() or check our blog post and the story of [how we guarantee ultra high performance on the server side]().
 //!
 //! The core of Iggy is a persisted append-only log data structure.
 //! The Iggy server is build around this data structure and consists of logic to allow consumers and producers to read and write data to and from that log.
-//! An overview of how this is implemented can be found on the [`website`]. Deep dives into design decisions and server internals to ensure high-performance standards are outlined in various extensive [`blog posts`].
+//! For more details visit our [`website`](). Deep dives into design decisions and server internals to ensure high-performance standards are outlined in various extensive [`blog posts`]().
 //!
 //! **This library is the Apache Iggy SDK for the Rust programming language.**
 //! SDKs for other programming languages can be found in [`core/foreign`] of the root repository on GitHub and respective package managers such as npm, maven, pypi and nuget.
@@ -34,6 +55,58 @@
 //!
 //! The following section will go into details on the different APIs the Rust SDK provides.
 //! For any question really feel free to join our [`discord`] where maintainers and contributers are happy to help.
+//!
+//! # Mental Model
+//! Apache Iggy Messages consist of 64 byte headers representing metadata and the actual message payload.
+//! Each message that arrives at the server is contiguously appended to a binary *.log* file, the **segment**.
+//! However, you might not want to send all messages to the same *.log* file since you are operating in an environemnt with domain boundaries,
+//! here messages belong to certain applications or teams.
+//! With **streams** and **topics** you can build two-level hirarchies to isolate messages into well defined boundaries.
+//! A stream is essentially a namespace and topics an entity that defines how a category of messages is stored (e.g. configuring compression and retention periods).
+//! All messages that belong to a specific topic are distributed across **partitions**.
+//! Partitions are not another logical hierarchy level. They exist to enable parallel processing in cases where many consumers need to read from the same topic.
+//! Within partitions you will find the *.log* file segement, such that on disk the outline will look something like this:
+//! ```text
+//! local_data/
+//! ├── info.json
+//! ├── state.messages
+//! └── streams/
+//!    └── 1/
+//!        └── topics/
+//!            └── 1/
+//!                └── partitions/
+//!                    └── 1/
+//!                        ├── 00000000000000000000.index
+//!                        └── 00000000000000000000.log
+//! ```
+//! We only touch briefly here on these concepts for completeness. You can find more details in the [architecture section](https://iggy.apache.org/docs/introduction/architecture/) on our website.
+//!
+//! Obviously, the server has quite elaborate implementations on how the messages end up in a .log file.
+//! The good thing is, that we do not need to take care of that. We just need to make sure to ship the message (payload) to the server's doorstep with some metadata (headers) so that that the server can route
+//! it to the correct .log file. The rest is abstracted away for us.
+//!
+//! To manipulate and interact with the server it exposes an interface called **commands**.
+//! **Commands** are a set of defined operations that the server understands and alters either its own state or the managed messages.
+//! The SDK can be understood as a wrapper around server commands and nothing else.
+//! Wrapping means, that we take the server commands and add additional functionality and logic that is frequently required when building message-streaming solutions.
+//! Basically, reuseable building blocks that would most likely be implemented by every application team otherwise.
+//!
+//! A complete server schema with all available commands can be found [here](https://iggy.apache.org/docs/server/schema/).
+//!
+//! # The different API tiers
+//! Users can decide between three different API tiers.
+//! TODO(haubur): table with columns, tier, name, purpose, design principle
+//!
+//!
+//! ## Persona: I want to ... where should I start?
+//! tba
+//!
+//! # The different API tiers
+//! ## High-level API
+//! The Iggy server implements and abstracts away the technical details to read and write to the append-only data structure as efficiently as possible.
+//! Producers (applications that want to write messages to the server) and consumers (applications that want to read messages from the server) can make use of server commands to talk to the server.
+//! Server commands are essentially the user interface of the server.
+//!
 //!
 //! ## Server Command Overview
 //! In its essence the Rust SDK wraps core feature of meassage streaming applications around Iggy server commands.
@@ -65,12 +138,13 @@
 //! - These are the domain clients (?)
 //! - What features are available here? e.g. polling strategies, partitioning strategies
 //! ### Streaming API
-//!
-//! - Low-level API implements the transport to directly access these commands.
+//!//! - Low-level API implements the transport to directly access these commands.
 //! - High-level API to instantiate consumers and producers.
 //! [^note]: The details of how that is done internally are not discussed here. Refer, e.g. to [`the blog`].
+//! [^note]: tba
+//! [^note]: Indeed Apache Iggy comes with many implemented [*connectors* or *sinks*]() where data buffered on iggy can be pushed to a database for long term storage.
 //!
-//! [`Apache Incubating Program`]: https://incubator.apache.org/
+//! [`Incubating Program`]: https://incubator.apache.org/
 //! [`Apache Software Foundation`]: https://www.apache.org/
 //! [`the blog`]: https://iggy.apache.org/blogs/2025/11/17/websocket-io-uring/
 //! [`core/foreign`]: https://github.com/apache/iggy/tree/master/foreign
